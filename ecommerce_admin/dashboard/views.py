4bbe797a7bb07e0e8029bf34f2c6e2d72c2567bb
George Hlongwane <georgehlongwane8@gmail.com>
2025-03-14 06:51:22 +0200

    Created views for displaying the templates we want

--- ecommerce_admin/dashboard/views.py
+++ ecommerce_admin/dashboard/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    return render(request, "index.html")
+
+def login_view(request):
+    return render(request, "login.html")
+
+def register(request):
+    return render(request, "register.html")
+
+# forgot password
+"""
+def forgot_password(request):
+    return render(request, "forgot_password.html") 
+"""
