9e43dcf0695fb78c6d64ff240aa4f2fc84707d5d
George Hlongwane <georgehlongwane8@gmail.com>
2025-03-14 06:06:17 +0200

    Configured dashboard/ Django app to settings file

--- ecommerce_admin/ecommerce_admin/settings.py
+++ ecommerce_admin/ecommerce_admin/settings.py
@@ -30,14 +30,11 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+INSTALLED_APPS = [ 'django.contrib.admin', 'django.contrib.auth', 
+    'django.contrib.contenttypes', 'django.contrib.sessions', 
+    'django.contrib.messages', 'django.contrib.staticfiles',# custom 
+    installed apps
+'rest_framework', 'dashboard',]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
